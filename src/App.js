036f5da7dc1b9b9806eb6df0ebc0da8b42709ca4
VitaPan <119960788+VitaPan@users.noreply.github.com>
2025-05-18 16:41:51 +0700

    added pie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
 function App() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Play games", 2],
+    ["Watch TV", 1],
+    ["Sleep", 10],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"600px"}
+    />
   );
 }
 
